a1d3e3b836c55089f5d5038d5c8e42815ea2a5b6
harinipechimuthu <131947088+harinipechimuthu@users.noreply.github.com>
2024-07-30 21:24:58 +0530

    Add files via upload

---
+++ hello .java
@@ -0,0 +1,9 @@
+// This is a basic Java program
+
+public class HelloWorld {
+    // The main method is the entry point of any Java application
+    public static void main(String[] args) {
+        // Print the string "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
